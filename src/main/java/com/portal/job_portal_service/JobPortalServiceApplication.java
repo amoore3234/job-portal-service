6baa74467d585563feb9495174a5665c74b0e8c4
Anthony Moore <moore.32@hotmail.com>
2025-11-15 11:14:26 -0800

    Initial commit

---
+++ src/main/java/com/portal/job_portal_service/JobPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.portal.job_portal_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portal/job_portal_service/JobPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.job_portal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
