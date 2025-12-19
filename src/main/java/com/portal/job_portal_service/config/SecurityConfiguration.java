1985c57dbb7c5935842552a37b770e3a9c9f03cf
Anthony Moore <moore.32@hotmail.com>
2025-12-18 22:12:53 -0800

    Add cors configuration source
    - Modified the SecurityConfiguration class to ensure requests can be made from the browser.
    - Added the RequestBody annotation to resolve request body issues.

--- src/main/java/com/portal/job_portal_service/config/SecurityConfiguration.java
+++ src/main/java/com/portal/job_portal_service/config/SecurityConfiguration.java
@@ -42,8 +42,24 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+      CorsConfiguration config = new CorsConfiguration();
+
+      config.setAllowedOrigins(List.of("http://localhost:5173"));
+      config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+      config.setAllowedHeaders(List.of("*"));
+      config.setAllowCredentials(true);
+
+      UrlBasedCorsConfigurationSource source =
+              new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", config);
+      return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(registry -> {
                 registry.requestMatchers(
--- src/main/java/com/portal/job_portal_service/controller/JobPortalController.java
+++ src/main/java/com/portal/job_portal_service/controller/JobPortalController.java
@@ -22,6 +22,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -58,7 +59,7 @@ public class JobPortalController {
         )
       )
     })
-  public ResponseEntity<?> createUser(User user) {
+  public ResponseEntity<?> createUser(@RequestBody User user) {
     user.setUserPassword(passwordEncoder.encode(user.getUserPassword()));
     user.setCreatedTimestamp(ZonedDateTime.now());
     userService.createUser(user);
